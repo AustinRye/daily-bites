bffebeab307983d976a608915e5bc482bd7e6755
Austin <ryeaustinw@gmail.com>
2026-01-24 20:25:49 -0800

    Refactor index into server and app

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import recipesRouter from './routes/recipes.routes.js';
+
+// Create an Express application
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Use routes
+app.use("/api/recipes", recipesRouter)
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,9 @@
 import 'dotenv/config';
-import express from 'express'
-import recipesRouter from './routes/recipes.routes.js';
-import pool from './configs/db.js';
-
-// Create an Express application
-const app = express();
+import app from './app.js'
 
 // Log the current environment
 const { NODE_ENV = 'development' } = process.env;
 console.log(`Running in ${NODE_ENV} mode`);
-
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// Use routes
-app.use("/api/recipes", recipesRouter)
 
 // Start the server
 const { PORT = 3000, HOST = 'localhost' } = process.env;
